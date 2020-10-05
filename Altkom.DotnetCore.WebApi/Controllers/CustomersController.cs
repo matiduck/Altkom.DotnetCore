47e5e7cfabb70c9a3efbe2288648adc487c42e3d
Mateusz Kaczor <matiduck@gmail.com>
2020-10-05 12:09:35 +0200

    neverending crud

--- Altkom.DotnetCore.WebApi/Controllers/CustomersController.cs
+++ Altkom.DotnetCore.WebApi/Controllers/CustomersController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Altkom.Dotnecore.IServices;
+using Altkom.DotnetCore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Altkom.DotnetCore.WebApi.Controllers
 {
     [Route("api/customers")]
+    //[ApiController] //typy referencyjne z body  
     public class CustomersController : ControllerBase
     {
         private readonly ICustomerService _customerService;
@@ -23,6 +25,54 @@ namespace Altkom.DotnetCore.WebApi.Controllers
             var customers = _customerService.Get();
 
             return Ok(customers);
+        }
+
+        [HttpGet("{id}", Name = nameof(GetCustomerById))]
+        public IActionResult GetCustomerById(Guid id)
+        {
+            var customer = _customerService.Get(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Customer customer)
+        {
+            _customerService.Add(customer);
+
+            return CreatedAtRoute(nameof(GetCustomerById), new { Id = customer.Id }, customer);
+        }
+
+        [HttpPut("{id}")]
+    
+        public IActionResult Put(Guid id,[FromBody] Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return BadRequest();
+            }
+
+            _customerService.Update(customer);
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")] //jsonPatch.com
+        public IActionResult Patch(Guid id, [FromBody] Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return BadRequest();
+            }
+
+            _customerService.Update(customer);
+
+            return NoContent();
         }
     }
 }
