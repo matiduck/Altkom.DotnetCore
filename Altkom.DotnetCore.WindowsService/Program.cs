ac878a319a3a48bbbdcb98f9e5836105c2b92c43
Mateusz Kaczor <matiduck@gmail.com>
2020-10-06 13:09:50 +0200

    Windows Service intial commit

---
+++ Altkom.DotnetCore.WindowsService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Altkom.DotnetCore.WindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
